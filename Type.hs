509ae565d78a83b8b4561ba7b5a515210f4d4f44
Stephen Dolan <mu@netsoc.tcd.ie>
2010-12-12 23:50:15 +0000

    ground instances as regular trees, with subtyping and normal forms

--- Type.hs
+++ Type.hs
@@ -2,9 +2,12 @@ module Type where
 
 import Data.Monoid
 import Data.Maybe
+import Data.Array
+import Data.Ord
 import Data.List
 import Data.Char
 import qualified Data.Set as S
+import qualified Data.IntMap as IM
 import Control.Monad
 import Control.Arrow
 import Control.Monad.ST
@@ -38,6 +41,11 @@ type LatticeMerge t = Variance -> t -> t -> t
 latticeSub :: Eq t => LatticeMerge t -> t -> t -> Bool
 latticeSub f a b = f Neg a b == a
 
+-- Same as latticeSub but gives <= or >= depending on variance parameter
+-- latticeRel f Pos is <=, latticeRel f Neg is <=
+latticeRel :: Eq t => LatticeMerge t -> Variance -> t -> t -> Bool
+latticeRel f Pos a b = latticeSub f a b
+latticeRel f Neg a b = latticeSub f b a
 
 data GroundSig = GroundSig [Constructor]
 data Constructor = Constructor String [Label] deriving (Show)
@@ -75,6 +83,7 @@ groundMerge (GroundSig cs) var a b =
 groundSub :: GroundSig -> Constructor -> Constructor -> Bool
 groundSub g = latticeSub (groundMerge g)
 
+groundRel g = latticeRel (groundMerge g)
 
 commonLabels (Constructor _ l1) (Constructor _ l2) =
     -- PERF (sorted lists/merge)
@@ -83,20 +92,156 @@ commonLabels (Constructor _ l1) (Constructor _ l2) =
       en = zip [0..]
 
 
+
+type Constructed a = (Constructor, [a])
+
+
+
+mergeIdentity :: Variance -> TypeTerm v
+mergeIdentity Neg = Const (groundTop, [])
+mergeIdentity Pos = Const (groundBot, [])
+mergeZero v = mergeIdentity (Neg `mappend` v)
+
+
+mergeConstructed :: Variance -> Constructed a -> Constructed a -> 
+                    (Constructor, [(Variance, Maybe a, Maybe a)])
+mergeConstructed v (c1@(Constructor _ lbls1), t1) (c2@(Constructor _ lbls2), t2) = 
+    -- PERF (sorted lists/merge)
+    let c'@(Constructor _ lbls') = groundMerge getGroundSig v c1 c2
+        find l x t = do { i<- findIndex (==l) x; return$ t !! i; }
+    in (c', [(labelVariance l, find l lbls1 t1, find l lbls2 t2) | l <- lbls'])
+
+getC n ts = 
+    let (GroundSig cs) = getGroundSig 
+    in (fromJust $ find (==Constructor n undefined) cs, ts)
+
+mkC n ts = Const $ getC n ts
+        
+
+
+
+data GroundInstance = GroundInstance [Constructed Int] deriving (Show,Eq)
+
+--mergeGroundInstance v (GroundInstance tx1) (GroundInstance tx2) =
+
+--sample =GroundInstance$ let c a x = (Constructor a [], x) in [c "c" [1], c"a" [1,3], c"b"[4,5], c"c"[4], c"c"[6], c"c"[7], c"d"[7]]
+sample =GroundInstance$ let c a x = (Constructor a [], x) in [c"a" [2,3], c"c"[2], c"b"[4,5], c"c"[4], c"c"[6], c"c"[7], c"d"[7]]
+
+f1 = GroundInstance$ [getC "->" [1,2], getC "Bot" [], getC "Top" []]
+f2 = GroundInstance$ [getC "->" [2,3], getC "->" [1,4], getC "Bot" [], getC "Top" []]
+
+toDot (GroundInstance g) = 
+    let edges = [(x,y,p) | (x,(_, succ)) <- zip [1..] g, (p,y) <- zip [1..] succ]
+        nodes = [(c,x) | (x,(Constructor c _, _)) <- zip [1..] g]
+        n x = "n"++show x
+        edgetxt = unlines$ [n x ++ " -> " ++ n y ++ "[label=" ++ show p++"]" | (x,y,p) <- edges]
+        nodetxt = unlines$ [n x ++ "[label = \""++s++":"++show x++"\"]" | (s,x) <- nodes]
+    in "digraph{\n"++nodetxt++edgetxt++"}\n"
+
+minimiseGroundInstance (GroundInstance txtable) = toGroundInstance (recSplit initPartition)
+    where
+      nstates = length txtable
+
+      states = array (1, nstates) (zip [1..] txtable)
+
+      getPartition :: [[Int]] -> Array Int Int
+      getPartition parts = array (1, nstates) $ concat $
+                           [[(state, partidx) | state <- partition] 
+                                | (partidx, partition) <- zip [1..] parts]
+      successors :: Int -> [Int]
+      successors = snd . (states!)
+
+
+      -- Initial (coarsest possible) partition
+      initPartition = map (map fst) $ groupOn (fst . snd) $ zip [1..] txtable
+
+      -- Keep splitting partitions until they stop changing
+      recSplit p = 
+          let newp = splitPartition p
+          in if newp == p then p else recSplit newp
+
+      -- Convert final partition into a minimal state-machine (term automaton)
+      toGroundInstance :: [[Int]] -> GroundInstance
+      toGroundInstance parts = 
+          let stateP p = (states ! (head (parts !! (p-1))))
+              con = fst . stateP
+              succS = snd . stateP
+              partlist = getPartition parts
+              succP = map (partlist!) . succS
+              
+              nparts = length parts
+              partMachine = array (1,nparts) [(i,succP i) | i <- [1..nparts]]
+
+              -- Find a relabeling of the reduced state machine (partMachine) to normal form
+              -- i.e. with all states numbered in depth-first-search order from the root
+              -- This has the side-effect of removing unreachable states, as they won't be
+              -- assigned a label
+              relabel :: Int -> IM.IntMap Int -> IM.IntMap Int
+              relabel node mapped =
+                  case (IM.lookup node mapped) of
+                    Just x -> mapped -- this node has already been visited
+                    Nothing -> -- visit this node
+                        let succs = partMachine ! node
+                            visitall = foldl (flip (.)) id $ map relabel succs
+                            newmap = IM.insert node (IM.size mapped + 1) mapped
+                        in visitall newmap
+
+              relabelmap = relabel 1 IM.empty
+              
+              lookuplabel node = fromJust $ IM.lookup node relabelmap
+                    
+              relabeledMachine = array (1,IM.size relabelmap) [(lookuplabel i, (con i, map lookuplabel $ succP i)) | i <- [1..nparts], i `IM.member` relabelmap]
+
+          in GroundInstance $ elems relabeledMachine
+              
+
+      splitPartition :: [[Int]] -> [[Int]]
+      splitPartition parts =
+          let partlist = getPartition parts
+              -- to find the list of successor partitions of a state, find
+              -- the partition of each successor state.
+              succP :: Int -> [Int]
+              succP = map (partlist!) . successors
+          in concat $ map (groupOn succP) $ parts
+
+      -- Partition a list into sets by function f s.t. x and y are in the same set
+      -- iff f x == f y
+      groupOn :: (Ord b) => (a -> b) -> [a] -> [[a]]
+      groupOn f = map (map snd) . 
+                  groupBy ((==) `on` fst) . 
+                  sortBy (comparing fst) . 
+                  map (f &&& id)
+      partition getP states = groupOn (getP . snd) states
+
+
+groundInstSub (GroundInstance tx1') (GroundInstance tx2') = 
+    let tx1 = array (1,length tx1') (zip [1..] tx1')
+        tx2 = array (1,length tx2') (zip [1..] tx2')
+        recsub v s1 s2 checked = 
+            let already = (s1,s2) `S.member` checked
+                hypothesis = (s1,s2) `S.insert` checked
+                (ctor, ts) = mergeConstructed Pos (tx1!s1) (tx2!s2)
+                validHead = groundRel (getGroundSig) v (fst (tx1!s1)) (fst (tx2!s2))
+                validSub (Pos,Just a,Just b) = recsub (v`mappend`Pos) a b hypothesis
+                validSub (Neg,Just a,Just b) = recsub (v`mappend`Neg) a b hypothesis
+                validSub _ = True
+            in already || (validHead && all validSub ts)
+        in recsub Pos 1 1 S.empty
+
 -- A type bound
 -- A note on "deriving Eq":
 --  Structural equality does not hold for general TypeTerms, for instance
 --  Merge [Merge[t]] == Merge[t]. However, we make sure never to directly
 --  construct a Merge type, rather we use "merge" to normalise the types
 --  so that structural equality holds.
-data TypeTerm v = Const Constructor [TypeTerm v]
+data TypeTerm v = Const (Constructed (TypeTerm v))
                 | TVar v
                 | Merge [TypeTerm v] -- in a Pos type, Merge is least-upper-bound
                   deriving (Eq,Ord)
 
 -- for debugging
 instance Show a => Show (TypeTerm a) where
-    show (Const (Constructor name _) ts) =
+    show (Const (Constructor name _, ts)) =
         if not (any isAlpha name) && length ts == 2 then
             show (ts !! 0) ++ " " ++ name ++ " " ++ show (ts !! 1)
         else
@@ -104,22 +249,6 @@ instance Show a => Show (TypeTerm a) where
     show (TVar v) = "$" ++ filter (/='"') (show v)
     show (Merge ts) = "{" ++ intercalate ", " (map show ts) ++ "}"
 
-
-mergeIdentity :: Variance -> TypeTerm v
-mergeIdentity Neg = Const groundTop []
-mergeIdentity Pos = Const groundBot []
-mergeZero v = mergeIdentity (Neg `mappend` v)
-
-
-mergeConstructed :: Variance -> TypeTerm v -> TypeTerm v -> 
-                    (Constructor, [(Variance, Maybe (TypeTerm v), Maybe (TypeTerm v))])
-mergeConstructed v (Const c1@(Constructor _ lbls1) t1) (Const c2@(Constructor _ lbls2) t2) = 
-    -- PERF (sorted lists/merge)
-    let c'@(Constructor _ lbls') = groundMerge getGroundSig v c1 c2
-        find l x t = do { i<- findIndex (==l) x; return$ t !! i; }
-    in (c', [(labelVariance l, find l lbls1 t1, find l lbls2 t2) | l <- lbls'])
-        
-        
 
 sortednub :: Ord a => [a] -> [a]
 sortednub = map head . group . sort
@@ -144,10 +273,10 @@ merge v xs =
           flatten (Merge x) = x
           flatten x = [x]
           sepVarConst (TVar _) = True
-          sepVarConst (Const _ _) = False
-          mergeConst t1 t2 = 
+          sepVarConst (Const _) = False
+          mergeConst (Const t1) (Const t2) = 
               let (ctor, ts) = mergeConstructed v t1 t2
-              in Const ctor (map mergeSubterm ts)
+              in Const (ctor, map mergeSubterm ts)
                  where
                    mergeSubterm (v', Nothing, Just y) = y
                    mergeSubterm (v', Just x, Nothing) = x
@@ -159,9 +288,6 @@ typeContained :: Ord v => Variance -> TypeTerm v -> TypeTerm v -> Bool
 typeContained Pos a b = merge Pos [a,b] == b
 typeContained Neg a b = merge Neg [a,b] == a
 
-mkC n ts =
-    let (GroundSig cs) = getGroundSig 
-    in Const (fromJust $ find (==Constructor n undefined) cs) ts
     
 -- Constraints are of the form PosType <= NegType
 data Constraint v = Constraint (TypeTerm v) (TypeTerm v) deriving Show
@@ -174,8 +300,8 @@ data ElementaryConstraint v = ConstraintVarVar v v
 
 checkElementaryConstraint (Constraint a b) = 
     case (a,b) of
-      (TVar v, Const _ _) -> Just $ ConstraintVarBound Neg v b
-      (Const _ _, TVar v) -> Just $ ConstraintVarBound Pos v a
+      (TVar v, Const _) -> Just $ ConstraintVarBound Neg v b
+      (Const _, TVar v) -> Just $ ConstraintVarBound Pos v a
       (TVar v1, TVar v2) -> Just $ ConstraintVarVar v1 v2
       _ -> Nothing
 
@@ -193,7 +319,7 @@ splitConstraint constraint@(Constraint a b) =
             (_, Merge terms) -> splitConstraints $ map (a `Constraint`) terms
             -- Here, we know there are no Merges or elementary constraints
             -- So, both sides must be constructed
-            (t1@(Const c1 _), t2@(Const c2 _)) -> 
+            (Const t1@(c1,_), Const t2@(c2,_)) -> 
                 -- Split a constraint between constructed terms into constraints
                 -- between common type parameters according to their variances
                 let (ctor, ts) = mergeConstructed Pos t1 t2
@@ -227,14 +353,14 @@ data SmallConstraint v = SConstraintVarVar v v
 
 checkSmallConstraint :: ElementaryConstraint v -> SmallConstraint v
 checkSmallConstraint (ConstraintVarVar v1 v2) = SConstraintVarVar v1 v2
-checkSmallConstraint (ConstraintVarBound v var (Const c ts)) = 
+checkSmallConstraint (ConstraintVarBound v var (Const (c,ts))) = 
     SConstraintVarBound v var (SmallTerm c (map fromMergeVar ts))
         where
           fromMergeVar (Merge vs) = map fromVar vs
           fromMergeVar (TVar v) = [v]
           fromVar (TVar v) = v
 
-fromSmallTerm v (SmallTerm c ms) = Const c (map (merge v . map TVar) ms)
+fromSmallTerm v (SmallTerm c ms) = Const (c, map (merge v . map TVar) ms)
 fromSmallConstraint (SConstraintVarVar v1 v2) = ConstraintVarVar v1 v2
 fromSmallConstraint (SConstraintVarBound v var t) = 
     ConstraintVarBound v var (fromSmallTerm v t)
